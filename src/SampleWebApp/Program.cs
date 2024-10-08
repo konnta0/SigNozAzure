5d9001d062d81b659d68416ea63b182848d032a9
konnta0 <konnta0@gmail.com>
2024-10-09 01:44:57 +0900

    always enabled swagger

--- src/SampleWebApp/Program.cs
+++ src/SampleWebApp/Program.cs
@@ -40,7 +40,7 @@ builder.Services.AddSingleton<Instrumentation>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
