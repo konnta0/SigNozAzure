0485411fae9dacaf92b63d966aed2b9b32d17c84
konnta0 <konnta0@gmail.com>
2024-10-16 01:00:50 +0900

    chore

--- src/SampleWebApp/Controllers/WeatherForecastController.cs
+++ src/SampleWebApp/Controllers/WeatherForecastController.cs
@@ -6,33 +6,29 @@ namespace SampleWebApp.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController(ILogger<WeatherForecastController> logger, Instrumentation instrumentation)
+    : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-    private readonly ActivitySource _activitySource;
-    private readonly Counter<int> _counter;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, Instrumentation instrumentation)
-    {
-        _logger = logger;
-        _activitySource = instrumentation.ActivitySource;
-        _counter = new Meter("WeatherForecastController").CreateCounter<int>("call_count", description: "The number of times the controller is called");
-    }
+    private readonly ActivitySource _activitySource = instrumentation.ActivitySource;
+    private readonly Counter<int> _counter = new Meter("WeatherForecastController").CreateCounter<int>("call_count", description: "The number of times the controller is called");
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        _logger.LogInformation("GetWeatherForecast called");
+        logger.LogInformation("GetWeatherForecast called");
         _counter.Add(1);
         const string activityName = "GetWeatherForecast";
         return Enumerable.Range(1, 5).Select(index =>
             {
-                using var activity = _activitySource.StartActivity(activityName);
+                using var activity = _activitySource.StartActivity(activityName,
+                    ActivityKind.Server,
+                    Activity.Current?.Context ?? default(ActivityContext)
+                );
                 activity?.SetTag("iteration", index);
                 var date = DateOnly.FromDateTime(DateTime.Now.AddDays(index));
                 activity?.SetTag("date", date);
